7300168baddb688227d6ba2c3a8638112d5b652f
Brian Illes <brianrilles@gmail.com>
2019-08-13 11:50:06 -0700

    adds step 5

--- book.rb
+++ book.rb
@@ -8,4 +8,13 @@ class Book
         @finished = false
         @count = 3
     end
+
+    def self.recommended_books
+        recommended = [
+            Book.new("The Well-Grounded Rubyist", "David A. Black"),
+            Book.new("Practical Object-Oriented Design in Ruby", "Sandi Metz"),
+            Book.new("Effective Testing with RSpec 3", "Myron Marston"),
+        ]
+        return recommended
+    end
 end
